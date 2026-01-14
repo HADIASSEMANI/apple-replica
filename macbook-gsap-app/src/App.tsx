5ae945316e4ba2dcecebe0ed8737a42d4a264a1e
Hadi Assemani <hadi.assemani@gmail.com>
2026-01-14 01:57:05 -0500

    Added Hero Section

--- macbook-gsap-app/src/App.tsx
+++ macbook-gsap-app/src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import NavBar from "./components/NavBar.tsx";
+import Hero from "./components/Hero.tsx";
 
 const App: React.FC = () => {
     return (
         <main>
             <NavBar />
+            <Hero />
         </main>
     )
 }

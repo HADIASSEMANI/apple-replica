de6327de4a811f2aeb4e71a9b3f4927ce6178105
Hadi Assemani <hadi.assemani@gmail.com>
2026-01-14 01:25:05 -0500

    Initial Commit: Set-Up and NavBar

---
+++ macbook-gsap-app/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import NavBar from "./components/NavBar.tsx";
+
+const App: React.FC = () => {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    )
+}
+
+export default App

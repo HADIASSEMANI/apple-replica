1f6e520b10ea969aa9cd169a447ee63bf0295498
Hadi Assemani <hadi.assemani@gmail.com>
2026-01-17 21:41:55 -0500

    Added 3D-models for the Macbook Products

--- macbook-gsap-app/src/App.tsx
+++ macbook-gsap-app/src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import NavBar from "./components/NavBar.tsx";
 import Hero from "./components/Hero.tsx";
+import ProductViewer from "./components/ProductViewer.tsx";
 
 const App: React.FC = () => {
     return (
         <main>
             <NavBar />
             <Hero />
+            <ProductViewer />
         </main>
     )
 }
---
+++ macbook-gsap-app/src/types/MacBookStoreData.ts
@@ -0,0 +1,6 @@
+export interface MacBookStoreData {
+    color: string;
+    scale: number;
+    setColor: (color: string) => void;
+    setScale: (scale: number) => void;
+}
